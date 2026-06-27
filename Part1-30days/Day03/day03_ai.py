286eb00f85e6a353ddec9ad69616726c3ace5d9c
RayaWolf <raya2026223@gmail.com>
2026-06-28 00:44:00 +0800

    success day03

---
+++ Part1-30days/Day03/day03_ai.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+# import os
+
+result=load_dotenv(
+    dotenv_path=r"..\Part1-30days\.env",
+    encoding="utf-8"
+            )
+
+# print(result)
+# print(os.getenv("OPENAI_API_KEY"))
+
+client = OpenAI()
+response = client.responses.create(
+model="gpt-5.5",
+input="Hello! This is my first AI program!")
+
+print(response.output_text)
---
+++ Part1-30days/Day03/notes.md
@@ -0,0 +1,10 @@
+## Day 3 - 
+## Date:
+ June 26, 2026
+---
+## What I did today
+- 
+
+## 🤔 My thoughts
+The programming world is like a huge toolbox. I don't have to build every tool myself. I need to understand what each tool does and how to combine them to create something new.
+
